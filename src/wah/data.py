c8553a3f4d58662124af2531c2123a743677e875
Juyeop Kim <juyeopkim@yonsei.ac.kr>
2024-02-05 17:21:14 +0900

    0.1.5 update (portion_dataset add)

--- src/wah/data.py
+++ src/wah/data.py
@@ -5,8 +5,13 @@ from .typing import (
     Optional,
 )
 
+import random
+
+from torch.utils.data import Subset
+
 __all__ = [
     "load_dataloader",
+    "portion_dataset",
 ]
 
 
@@ -22,3 +27,42 @@ def load_dataloader(
         num_workers=config["num_workers"],
         persistent_workers=True if config["num_workers"] > 0 else False,
     )
+
+
+def portion_dataset(
+        dataset: Dataset,
+        portion: float,
+        balanced: Optional[bool] = True,
+        random_sample: Optional[bool] = False,
+) -> Subset:
+    if balanced:
+        assert hasattr(dataset, "targets"), \
+            f"Unable to create a balanced dataset as there are no targets in the dataset."
+
+        targets = dataset.targets
+        classes = list(set(targets))
+
+        indices = []
+
+        for c in classes:
+            c_indices = [i for i, target in enumerate(targets) if target == c]
+            num_c = int(len(c_indices) * portion)
+
+            if random_sample:
+                c_indices = random.sample(c_indices, num_c)
+
+            else:
+                c_indices = c_indices[:num_c]
+
+            indices += c_indices
+
+    else:
+        num_data = int(len(dataset) * portion)
+
+        if random_sample:
+            indices = random.sample([i for i in range(len(dataset))], num_data)
+
+        else:
+            indices = [i for i in range(len(dataset))][:num_data]
+
+    return Subset(dataset, indices)
