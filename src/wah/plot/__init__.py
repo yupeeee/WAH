1bf367337714b28f81bc81e09e183545324db3c2
yupeeee <juyeopkim@yonsei.ac.kr>
2024-05-22 17:26:02 +0900

    0.1.66; TensorBoard add

--- src/wah/plot/__init__.py
+++ src/wah/plot/__init__.py
@@ -1,7 +1,9 @@
 from .hist import *
 from .scatter import *
+from .tensorboard import TensorBoard
 
 __all__ = [
     "HistPlot2D",
     "ScatterPlot2D",
+    "TensorBoard",
 ]
---
+++ src/wah/plot/tensorboard.py
@@ -0,0 +1,5 @@
+from torch.utils.tensorboard import SummaryWriter as TensorBoard
+
+__all__ = [
+    "TensorBoard",
+]
