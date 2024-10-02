5b83e5ac4f3103f8fb40220ba99a7714558c8dca
yupeeee <juyeopkim@yonsei.ac.kr>
2024-10-02 16:50:35 +0900

    1.7.4; JacobianSigVals add

--- src/wah/riemann/__init__.py
+++ src/wah/riemann/__init__.py
@@ -5,6 +5,9 @@ from .grad import (
     compute_jacobian,
     compute_hessian,
 )
+from .jacobian_sigvals import (
+    JacobianSigVals,
+)
 
 __all__ = [
     # geodesic
@@ -12,4 +15,6 @@ __all__ = [
     # grad
     "compute_jacobian",
     "compute_hessian",
+    # jacobian_sigvals
+    "JacobianSigVals",
 ]
---
+++ src/wah/riemann/jacobian_sigvals.py
@@ -0,0 +1,95 @@
+import lightning as L
+import torch
+from torch.func import jacrev
+
+from .. import utils
+from ..classification.datasets import to_dataloader
+from ..classification.train.utils import load_accelerator_and_devices
+from ..typing import Dataset, Devices, Dict, List, Module, Optional, Tensor
+
+__all__ = [
+    "JacobianSigVals",
+]
+
+
+class Wrapper(L.LightningModule):
+    def __init__(
+        self,
+        model: Module,
+        res_dict: Dict[str, List],
+    ) -> None:
+        super().__init__()
+
+        self.model = model
+        self.res_dict = res_dict
+
+    def test_step(self, batch: Tensor, batch_idx):
+        batch_size = len(batch)
+        input_dim = batch[0].numel()
+
+        # enable gradients
+        with torch.inference_mode(False):
+            batch = batch.clone().detach().requires_grad_(True)
+            J: Tensor = jacrev(self.model)(batch)
+
+        assert J.requires_grad
+        J = J.detach().reshape(batch_size, -1, input_dim)
+
+        sigvals: Tensor = torch.linalg.svdvals(J)
+
+        self.res_dict["sigval"].append(sigvals.cpu())
+
+    def on_test_epoch_end(self) -> None:
+        pass
+
+
+class JacobianSigVals:
+    def __init__(
+        self,
+        batch_size: int,
+        num_workers: int,
+        seed: Optional[int] = None,
+        devices: Optional[Devices] = "auto",
+    ) -> None:
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.seed = seed
+        self.devices = devices
+
+        utils.random.seed(self.seed)
+        accelerator, devices = load_accelerator_and_devices(self.devices)
+
+        self.runner = L.Trainer(
+            accelerator=accelerator,
+            devices=devices,
+            logger=False,
+            max_epochs=1,
+            log_every_n_steps=None,
+            deterministic="warn" if self.seed is not None else False,
+        )
+        self.config = {
+            "batch_size": self.batch_size,
+            "num_workers": self.num_workers,
+        }
+
+    def __call__(
+        self,
+        model: Module,
+        dataset: Dataset,
+    ) -> Tensor:
+        res_dict = {
+            "sigval": [],
+        }
+        model = Wrapper(model, res_dict)
+        dataloader = to_dataloader(
+            dataset=dataset,
+            train=False,
+            **self.config,
+        )
+
+        self.runner.test(
+            model=model,
+            dataloaders=dataloader,
+        )
+
+        return torch.cat(res_dict["sigval"], dim=0)
