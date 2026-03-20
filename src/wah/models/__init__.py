147a84f44c15896fee4e21b4e4eceffa3a03973a
yupeeee <juyeopkim@yonsei.ac.kr>
2026-03-20 00:48:29 +0000

    1.21.4; CLIP added in __init__

--- src/wah/models/__init__.py
+++ src/wah/models/__init__.py
@@ -1,6 +1,6 @@
 from .classification import ClassificationModel
 from .diffusion import RealisticVision, StableDiffusion
-from .feature_extraction import SSCD
+from .feature_extraction import CLIP, SSCD
 from .module import getargs, getattrs, getmod, getname, setmod, summary
 
 __all__ = [
@@ -10,6 +10,7 @@ __all__ = [
     "StableDiffusion",
     "RealisticVision",
     # feature extraction
+    "CLIP",
     "SSCD",
     # module
     "getargs",
