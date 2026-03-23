55c3ef1a93b69b044062765c37417bbf85ef5523
yupeeee <juyeopkim@yonsei.ac.kr>
2026-03-23 00:56:57 +0000

    1.21.5; add to, half to fid

--- src/wah/metrics/fid/__init__.py
+++ src/wah/metrics/fid/__init__.py
@@ -169,6 +169,14 @@ class FID:
         self.device = device
         self.use_half = use_half
         self.verbose = verbose
+    
+    def to(self, device: torch.device) -> "FID":
+        self.device = device
+        return self
+    
+    def half(self) -> "FID":
+        self.use_half = True
+        return self
 
     def __call__(
         self,
