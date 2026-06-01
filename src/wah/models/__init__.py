45abdfc6f2e10b4fee42fea4c2d1b3ffe78fb44d
yupeeee <juyeopkim@yonsei.ac.kr>
2026-06-01 15:38:22 +0000

    1.22.3; feature_extraction add

--- src/wah/models/__init__.py
+++ src/wah/models/__init__.py
@@ -1,5 +1,10 @@
 from .diffusion import DiffusionModel
+from .feature_extraction import CLIP, SSCD
 
 __all__ = [
+    # diffusion
     "DiffusionModel",
+    # feature_extraction
+    "CLIP",
+    "SSCD",
 ]
---
+++ src/wah/models/feature_extraction/__init__.py
@@ -0,0 +1,7 @@
+from .clip import CLIP
+from .sscd import SSCD
+
+__all__ = [
+    "CLIP",
+    "SSCD",
+]
